7c4451ede313d53bd2ec03f0ce588a59d691d67f
SimonVdvr <56001991+SimonVdvr@users.noreply.github.com>
2020-02-01 22:59:19 +0100

    Suite de la page creerSpectacle

--- js/spectacle.js
+++ js/spectacle.js
@@ -1,5 +1,9 @@
+/**
+ * Envoie une requête AJAX pour récupérer les villes où un spectacle est prévu
+ * le nombre de dates possibles, et le nombre d'inscrits.
+ */
+
 function chargerVilles(){
-    console.log("Récupération des villes...");
     $.ajax({
         method:"GET",
         url:"./minControleur/dataSpectacle.php",
@@ -7,23 +11,23 @@ function chargerVilles(){
             action:"chargerVilles"
         },
         success:function(oRep){
-            console.log("Succès");
-            console.log(oRep);
-            afficherVilles(oRep);
+            afficherResumeVilles(oRep);
         },
         error : function(oRep){
-            console.log("Erreur");
-            console.log(oRep);
+            console.log("Erreur lors de la récupération des villes.");
         } 
     });
 }
 
-function afficherVilles(rep){
-    console.log("Affichage des villes...");
+/**
+ * Affiche toutes les villes où des spectacles sont en cours de création
+ * sous la forme Ville | Nb de dates | Nb d'inscrits (toutes dates confondues)
+ * 
+ * @param {*} rep tableau [nomDeLaVille,NbDeDates,NbDInscrits]
+ */
+function afficherResumeVilles(rep){
     var tab = JSON.parse(rep);
-    console.log(rep);
     tab.forEach(element => {
-        console.log("On crée la case : "+element);
         var currVille = $("<div/>").addClass("ville")
             .append($("<table/>")
                 .append($("<td/>").html(element[0]))
@@ -36,14 +40,72 @@ function afficherVilles(rep){
     });
 }
 
-/*
 
-    <div class="ville">
-            <table >
-                <td>Paris</td>
-                <td>12 Date(s)</td>
-                <td>610 Inscrit(s)</td>
-            </table>
-        </div>
+/**
+ * Fonction qui vérifie si la vile dont le nom est passé en paramètre est dans la BDD où non.
+ * @param {*} nom Nom de la ville dont on veut vérifier l'abscence dans la BDD
+ */
+function verifVille(nomVille){
+    $.ajax({
+        method:"GET",
+        url:"./minControleur/dataSpectacle.php",
+        data:{
+            action:"verifVilleNom",
+            nom:nomVille
+        },
+        success:function(oRep){
+            var cont_info;
+            var info;
+            var classes;
+            oRep = JSON.parse(oRep);
+            
+            if(oRep[1] == 0){
+                classes = "alert alert-warning";
+                info = "<b>"+oRep[0]+" </b>  a déja des dates!<br> Cliquez ici pour les modifier / en ajouter"
+            }
+            else{
+                classes = "alert alert-success";
+                info = "<b>"+oRep[0]+" </b> n'a pas encore de dates! <br>Veuillez maintenant <b>choisir des dates </b>!"
+            }
+            window.setTimeout(function(){
+                //La ville est déja dans la BDD => On affiche une erreur comportant un lien vers la page de modification de la ville
+                $("#entrerVille").children().last().hide();
+                $("#validerEntreeVille").show(200);
+                cont_info = $("<div/>").addClass(classes).attr("role","alert")
+                .html(info)
+                .css("font-size","smaller").hide();
+                $("#entrerVille").append(cont_info);
+                $("#entrerVille").children().last().slideDown(500);
+                
+            },200);
+        },
+        error : function(oRep){
+            console.log("Erreur lors de la récupération des villes.");
+        } 
+    });
+}
 
-*/
+$(document).ready(function(){
+    $("#validerEntreeVille").click(function(){
+        var currLoader = loader.clone(1);
+        console.log(currLoader[0]);
+        console.log($("#entrerVille").children().last());
+        if(!$("#entrerVille").children().last().is($(this))){
+            $("#entrerVille").children().last().remove();
+        }
+        $(this).hide()
+        $("#entrerVille").append(currLoader[0]);
+        
+        verifVille($("#champTxtVille").val())
+            
+            
+    });
+    $("#champTxtVille").on("input",function(){
+        
+        if($(this).val() == "")
+        {
+            $("#validerEntreeVille").prop("disabled",true);
+        }
+        else $("#validerEntreeVille").prop("disabled",false);
+    });
+});
--- templates/creerSpectacle.php
+++ templates/creerSpectacle.php
@@ -16,17 +16,24 @@
 </h3>
 
 <div class="lead">
-    <span class="gras">Entrer une ville où créer un Spectacle : </span><br>
-    <?php 
-        mkInput("text","nomVille","","placeholder='Entrer le nom de la ville' class='form-control'");
-    ?>
-    <br><span class="gras">Villes où des spectacles ont déja été proposés : </span>
+    <div id="entrerVille">
+        <span class="gras">Entrer une ville où créer un Spectacle : </span><br>
+        <?php 
+            mkInput("text","nomVille","","placeholder='Entrer le nom de la ville' class='form-control' id='champTxtVille'");
+        ?>
+        <button disabled id="validerEntreeVille" class="btn btn-primary" style="margin-bottom:3px;">Valider</button>
+    </div>
     <div id="listeVilles">
+        <br><span class="gras">Villes où des spectacles ont déja été proposés : </span>
+        Cliquez pour modifier/ajouter des dates, ou consulter les détails pour cette ville
+        <br>(Liste des inscrits par dates, etc)
+    
         <script>
             chargerVilles();
         </script>
         
     </div>
+    
 </div>
 
     
