f9cbe288d242cbbca81d2205b3d77d37c4eebb5f
Cromach <clementboyaval@gmail.com>
2020-02-05 17:45:20 +0100

    Ajout des CSS/JS

--- templates/header.php
+++ templates/header.php
@@ -38,11 +38,10 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
 
     </script>
 	<link rel="icon" href="ressources/logo.png">
-	<script src="https://kit.fontawesome.com/05f96bf93f.js"></script>
+	<!--<script src="https://kit.fontawesome.com/05f96bf93f.js"></script>-->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
 	<script src="js/jquery.js"></script>
 	<script src="bootstrap/js/bootstrap.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
 	
 	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -52,10 +51,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
 	<!-- Liaisons aux fichiers css de Bootstrap -->
 
 	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href="css/bibliio.css" rel="stylesheet" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css">
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
 	<link href="css/sticky-footer.css" rel="stylesheet" />
 	<!--[if lt IE 9]>
 	  <script src="js/html5shiv.js"></script>
