dfcb78bb4eba59c0825e0c7170d7408e48f7214e
jasonstrs <56001431+jasonstrs@users.noreply.github.com>
2020-02-07 13:57:33 +0100

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -2,7 +2,7 @@
 // Si la page est appelée directement par son adresse, on redirige en passant pas la page index
 if (basename($_SERVER["PHP_SELF"]) != "index.php")
 {
-	header("Location:../index.php?view=login");
+	header("Location:../index.php?view=accueil");
 	die("");
 }
 
