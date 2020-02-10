4586d9ca57c7f7c3ac08bb91145f3bc48e6c70f3
MaNOnNaaAaO <60438606+MaNOnNaaAaO@users.noreply.github.com>
2020-02-10 14:38:53 +0100

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
 <?php
 
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-if (basename($_SERVER["PHP_SELF"]) != "index.php")
-{
-	header("Location:../index.php");
-	die("");
-}
+	// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+	if (basename($_SERVER["PHP_SELF"]) != "index.php")
+	{
+		header("Location:../index.php");
+		die("");
+	}
 
 ?>
 
@@ -13,9 +13,10 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 </div>
 <!-- fin du content --> 
 
-<!-- fin du wrap -->
+<!-- fin du wrap (dans le header) -->
 </div>
 
+<<<<<<< Updated upstream
 <div id="footer">
   <div class="container">
    	 <p class="text-muted credit">
@@ -30,6 +31,31 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	</p>
   </div>
 </div>
+=======
+
+
+<nav id="footer" class="navbar fixed-bottom navbar-expand-lg navbar-light" style="background-color : #c8912a">
+
+      	<ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+      		<li class="nav-item">
+		        <span class="navbar-text  mx-5">adresse email à modifier</span>
+		    </li>
+	        <li class="nav-item">
+				<img class="mx-2" src="ressources/tipeee.png" height="30px" width="auto"/>
+				<img class="mx-2" src="ressources/twitter.png" height="30px" width="auto"/>
+				<img class="mx-2" src="ressources/facebook.png" height="30px" width="auto"/>
+				<img class="mx-2" src="ressources/discord.png" height="30px" width="auto"/>
+			</li>
+		</ul>
+
+</nav>
+
+>>>>>>> Stashed changes
 
 </body>
+
+<footer>
+<script src="js/bibliio.js"></script>
+</footer>
+
 </html>
