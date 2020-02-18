bdf814c7d68384db615b072f90373cbadd850e19
SimonVdvr <56001991+SimonVdvr@users.noreply.github.com>
2020-02-18 16:32:34 +0100

    ça avance, ça avance
    Bientôt fini la page de création d'un spectacle
    + Ajout de nouvelles classes dans le main.css -> centre pour text-align center, nonSelectionnable, gras, pointer
    + Ajout d'une fonction de création de modal Bootstrap dans main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,57 @@ optionsAffichageDate = { weekday: 'long', year: 'numeric', month: 'long', day: '
 var loader = $("<img>").attr({"src":"./ressources/loading.gif"}).css({"width":"50px"});
 
 
+var modal = $("<div/>").addClass("modal fade").attr({"tabindex":"-1","role":"dialog"}).append(
+                $("<div/>").addClass('modal-dialog').attr("role","document").append(
+                    $("<div/>").addClass("modal-content").append(
+                        $("<div/>").addClass("modal-header").append(
+                            $("<h5/>").addClass("modal-title")
+                        )
+                    )
+                )
+            )
+
+
+/**
+ * Fonction permettant de créer un modal bootstrap
+ * /!\ Crée un modal "simple" => Que du texte pour le titre, 1 seul bouton de confirmatio net un d'annulation dans le footer
+ * Ne pas utiliser cette fonction pour créer des modal plus complexes
+ * @param {string} id Permet de donner un id au modal, afin de pouvoir le déclencher avec un bouton
+ * <button type="button" class="btn btn-primary" data-toggle="modal" data-target="id">
+ * @param {string} titre Titre du modal
+ * @param {string} contenu Contenu HTML du corps du modal
+ * @param {string} confirmation Texte du bouton de confirmation
+ * @param {string} couleur Couleur du bouton de confirmation (en classes bootstrap, ex: btn-warning pour jaune, btn-primary pour bleu,btn-danger pour rouge)
+ * @param {object} requete La requete ajax à lancer lors de la confirmation.
+ * 
+ */
+function creerModal(id,titre,contenu,confirmation,couleur,requete){
+var modal = $("<div/>").addClass("modal fade").attr({"tabindex":"-1","role":"dialog","id":id,"aria-labelledby":id,"aria-hidden":"true"}).append(
+                $("<div/>").addClass('modal-dialog').attr("role","document").append(
+                    $("<div/>").addClass("modal-content").append(
+                        $("<div/>").addClass("modal-header").append(
+                            $("<h5/>").addClass("modal-title").attr("id",id).html(titre)
+                        ).append(
+                            $("<button/>").addClass("close").attr({"type":"button","data-dismiss":"modal","aria-label":"Close"}).append(
+                                $("<span/>").html("&times;").attr("aria-hidden","true")
+                            )
+                        )
+                    ).append(
+                        $("<div/>").addClass("modal-body").html(contenu)
+                    ).append(
+                        $("<div/>").addClass("modal-footer").append(
+                            $("<button/>").addClass("btn btn-secondary").attr({"type":"button","data-dismiss":"modal"}).html("Fermer")
+                        ).append(
+                            $("<button/>").addClass("btn "+couleur).attr({"type":"button"}).html(confirmation).click(function(){
+                                $.ajax({
+                                    requete
+                                })
+                            })
+                        )
+                    
+                    )
+                )
+            );
+
+$("body").append(modal);
+}
--- templates/planiSpectacles.php
+++ templates/planiSpectacles.php
@@ -36,16 +36,16 @@
     <div id="choisirDates">
         <br><span class="gras"></span>
         <?php
+            echo "Ville : ";
             mkInput("text","nomVilleBis",""
-            ,"placeholder='Entrer le nom de la ville' class='form-control' id='champTxtVilleBis'");
+            ,"placeholder='Entrer le nom de la ville' class='form-control champNomDesc' id='champTxtVilleBis'");
+            echo "Description du spectacle : ";
             mkInput("text","descSpec",""
-            ,"placeholder='Entrer la description du spectacle' class='form-control' id='champTxtDescSpectacle'");
+            ,"placeholder='Entrer la description du spectacle' class='form-control champNomDesc' id='champTxtDescSpectacle'");
         ?>
-        <br><span class="gras">Choix de la méthode de sélection des dates : </span>
-        <div class="btn-group" role="group" aria-label="Choix de la sélection">
-            <button type="button" class="btn btn-primary methodeChoixDates">Selectionner des dates une par une</button>
-            <button type="button" class="btn btn-primary methodeChoixDates">Selectionner une plage de dates</button>
-        </div>
+            <button type="button" id="boutonSelectDates" class="btn btn-primary methodeChoixDates" disabled>Selectionner des dates</button>
+            <br><div id="selectionDesDates"></div>
+            <br><button type="button" id="boutonValiderDates" class="btn btn-primary methodeChoixDates" data-toggle="modal" data-target="#modalConfirmDates">Valider ces dates</button>
     </div>
     
 </div>
