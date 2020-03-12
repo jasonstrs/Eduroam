1585b007d58ec9d38348fabf5b22888f7fa6a8aa
SimonVdvr <56001991+SimonVdvr@users.noreply.github.com>
2020-03-12 13:34:58 +0000

    Azerty12 tmtc

--- templates/statsSpectacles.php
+++ templates/statsSpectacles.php
@@ -1,17 +1,18 @@
 <script src='js/statsSpectacles.js'></script>
 
 <div id="accordion">
-  <div class="card pointer" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+  <div class="card pointer" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
     <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
+      <h5 class="mb-0 nonSelectionnable">
         <button class="btn btn-link">
-          Dates en attente
+          Dates en attente 
         </button>
+        <i>Cliquer pour développer</i>
         <span class="nbDates" id="nbDatesEnAttente">Chargement <img src="./ressources/loading.gif" /></span>
       </h5>
     </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
       <div class="card-body">
         Ici, on affiche les dates en attente.
       </div>
@@ -19,10 +20,11 @@
   </div>
   <div class="card pointer" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
     <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
+      <h5 class="mb-0 nonSelectionnable">
         <button class="btn btn-link collapsed" >
-          Dates validées
+          Dates validées 
         </button>
+        <i>Cliquer pour développer</i>
         <span class="nbDates" id="nbDatesValidees">Chargement <img src="./ressources/loading.gif" /></span>
       </h5>
     </div>
