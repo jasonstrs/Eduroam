8a18decf4dd25ab1fbf8aa8bc17a99d85beaee6b
Cromach <clementboyaval@gmail.com>
2020-04-07 16:32:23 +0200

    User/role

--- js/users.js
+++ js/users.js
@@ -1,25 +1,43 @@
 $("#userSearch").keyup(function(){
-    console.log("On a changé le truc");
-    console.log($("#userSearch").val());
     if(!$("#userSearch").val().length==0) {
-        $.ajax({
-            type: "POST",
-            url: "./minControleur/dataUsers.php",
-            data: {"action": "search", "tag": $("#userSearch").val()},
-            success: function(oRep){
-                console.log(oRep);
-                $("#userResult").empty();
-                for(let i=0; i<oRep.length; i++) {
-                    addResult(oRep[i]);
-                }
-            },
-            dataType: "json"
-        });
+        autocomplete();
     }
     else $("#userResult").empty();
 })
 
-function addResult(obj) {
+function addResult(obj, number) {
     let div = `<div class="userResult" id="user`+obj.idU+`">`+ obj.prenom +` `+ obj.nom +` (`+ obj.email + `)</div>`;
     $("#userResult").append(div);
+    $("#user"+obj.idU).css({ 
+        top: parseInt((number)*41)
+    })
 }
+
+function autocomplete(){
+    $.ajax({
+        type: "POST",
+        url: "./minControleur/dataUsers.php",
+        data: {"action": "search", "tag": $("#userSearch").val()},
+        success: function(oRep){
+            //console.log(oRep);
+            $("#userResult").empty();
+            for(let i=0; i<oRep.length; i++) {
+                addResult(oRep[i], i);
+            }
+        },
+        dataType: "json"
+    });
+}
+
+$("#userSearch").focusin(function(){
+    if(!$("#userSearch").val().length==0) {
+        autocomplete();
+    }
+})
+
+$(".userResult").click(function(){
+})
+
+$("#userSearch").focusout(function(){
+    $("#userResult").empty();
+})
